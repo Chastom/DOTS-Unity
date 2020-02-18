15e9485e3d360dc443b379570f7b2d024877a460
Matas Strukcinskas <matas.strukcinskas@unity3d.com>
2020-02-18 13:27:14 +0200

    Updates on HP bar

--- Assets/PlayerDataSingletone.cs
+++ Assets/PlayerDataSingletone.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PlayerDataSingletone : MonoBehaviour
 {
     public static PlayerDataSingletone instance = null;
-    public int HP = 100;
     public GameObject HealthBar;
+    public float HP = 100;
+    public float HealthPointsIncrementSize = 1;
+    public float HealthPointsIncrementTime = 2;
     public int EnemiesKilled = 0;
+
+    public GameObject GameOverText;
+
+    private float DummyTimer;
 
     void Awake()
     {
@@ -17,9 +23,43 @@ public class PlayerDataSingletone : MonoBehaviour
             Destroy(gameObject);
     }
 
-    public void InflictDamage(int damage)
+    public void FixedUpdate()
+    {
+        DummyTimer -= Time.fixedDeltaTime;
+
+        if (DummyTimer <= 0)
+        {
+            AddHealth(HealthPointsIncrementSize);
+            DummyTimer = HealthPointsIncrementTime;
+        }
+    }
+
+    public void AddHealth(float health)
+    {
+        if (HP + health <= 100)
+            HP += health;
+        else
+            HP = 100;
+
+        UpdateHealthBar();
+    }
+
+    public void InflictDamage(float damage)
     {
         HP -= damage;
-        HealthBar.transform.localScale = new Vector3( 1f, -((float)HP / 100));
+
+        if (HP > 0)
+            UpdateHealthBar();
+        else
+        {
+            UpdateHealthBar();
+            Time.timeScale = 0; // Stop game xd
+            GameOverText.SetActive(true);
+        }
+    }
+
+    private void UpdateHealthBar()
+    {
+        HealthBar.transform.localScale = new Vector3(1f, -(HP / 100));
     }
 }
